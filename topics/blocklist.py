05f71a7a1471df23e3152c6f3de765272e9e0fb2
Gaurav Anand <gauravanand25@gmail.com>
2021-05-13 15:20:56 -0400

    create blocklist

---
+++ topics/blocklist.py
@@ -0,0 +1,13 @@
+"""
+Topics containing words in the following dictionary would be removed. Please write in lowercase.
+"""
+exclude_topics_containing_words = {
+  "ears"
+}
+
+"""
+Exact topic names would be removed. Please write in lowercase
+"""
+exclude_these_topics = {
+  "gun"
+}
